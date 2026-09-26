90e3f260c7e89757338c0624a354911ab2efa63b
Debajit <debajit.bcpl@gmail.com>
2026-09-26 18:05:44 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "ppu-dashboard-v10";   // 🔁 change version on every update
+const CACHE_NAME = "ppu-dashboard-v12";   // 🔁 change version on every update
 
 const urlsToCache = [
     "./",
-    "./index.html",
+    "./index.html?v=12",
     "./manifest.json",
     "./service-worker.js",
     "https://raw.githubusercontent.com/debajitd1292/ppu/main/logo.png"
